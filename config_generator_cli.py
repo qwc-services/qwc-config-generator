87660f494f2555cebae3f128a21a93b6446e31ef
Sandro Mani <manisandro@gmail.com>
2023-05-09 12:55:50 +0200

    config_file -> args.config_file

--- config_generator_cli.py
+++ config_generator_cli.py
@@ -57,7 +57,7 @@ except Exception as e:
 logger = Logger()
 
 # create ConfigGenerator
-generator = ConfigGenerator(config, logger, os.path.dirname(config_file))
+generator = ConfigGenerator(config, logger, os.path.dirname(args.config_file))
 if args.command == 'all':
     generator.write_configs()
     generator.write_permissions()
