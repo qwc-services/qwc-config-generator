71cbfc38905e913748eea89ed02a60703a835dd3
CebullaD <daniel.cebulla@jena.de>
2024-05-30 00:29:06 +0200

    adding permission generation

--- src/config_generator/document_service_config.py
+++ src/config_generator/document_service_config.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 
+from .permissions_query import PermissionsQuery
 from .service_config import ServiceConfig
 
 
@@ -35,3 +36,44 @@ class DocumentServiceConfig(ServiceConfig):
         config['resources'] = resources
 
         return config
+
+    def permissions(self, role):
+        """Return service permissions for a role.
+
+        :param str role: Role name
+        """
+        # NOTE: use ordered keys
+        permissions = OrderedDict()
+
+        if 'permissions' not in self.service_config:
+            # collect permissions from ConfigDB
+            with self.config_models.session() as session:
+                # helper method alias
+                permitted_resources = self.permissions_query.permitted_resources
+
+                # collect role permissions from ConfigDB
+                document_templates = permitted_resources(
+                    'document_templates', role, session
+                ).keys()
+                permissions['document_templates'] = sorted(list(document_templates))
+        else:
+            # use permissions from additional service config if present
+            self.logger.debug("Reading permissions from tenantConfig")
+
+            # additional service config
+            cfg_permissions = self.service_config.get('permissions', [])
+
+            for role_permissions in cfg_permissions:
+                # find role in permissions
+                if role_permissions.get('role') == role:
+                    # get permissions for role directly from service config
+                    document_permissions = role_permissions.get(
+                        'permissions', {}
+                    )
+                    if 'document_templates' in document_permissions:
+                        permissions['document_templates'] = \
+                            document_permissions['document_templates']
+
+                    break
+
+        return permissions
