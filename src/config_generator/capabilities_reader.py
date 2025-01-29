427f171e938c8dff78afa9263a7783dbca363565
Sandro Mani <manisandro@gmail.com>
2025-01-29 11:44:12 +0100

    Fix one more missing 'Exception as e'

--- src/config_generator/capabilities_reader.py
+++ src/config_generator/capabilities_reader.py
@@ -690,7 +690,7 @@ class CapabilitiesReader():
                     os.makedirs(os.path.dirname(cache_file), exist_ok=True)
                     with open(cache_file, "w") as fh:
                         fh.write(document.decode('utf-8'))
-                except:
+                except Exception as e:
                     self.logger.debug("Failed to store WFS DescribeFeatureType for %s in cache: %s" % (full_url, str(e)))
 
             # parse WFS Capabilities XML
