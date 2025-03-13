23460051edefebaf08724001ada784a11a47f0f6
Sandro Mani <manisandro@gmail.com>
2025-03-13 10:53:31 +0100

    Honour map3d config item

--- src/config_generator/map_viewer_config.py
+++ src/config_generator/map_viewer_config.py
@@ -529,6 +529,7 @@ class MapViewerConfig(ServiceConfig):
         self.set_optional_config(cfg_item, 'themeInfoLinks', item)
         self.set_optional_config(cfg_item, 'layerTreeHiddenSublayers', item)
         self.set_optional_config(cfg_item, 'predefinedFilters', item)
+        self.set_optional_config(cfg_item, 'map3d', item)
 
         if not cfg_item.get('wmsOnly', False):
             item['thumbnail'] = self.get_thumbnail(cfg_item, service_name, cap, assets_dir)
