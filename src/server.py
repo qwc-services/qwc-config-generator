52977c24ac7bf165015e5cf3c3524238f1709ac2
Sandro Mani <manisandro@gmail.com>
2025-04-29 17:44:33 +0200

    Stream log entries as they are written in generate_configs response

--- src/server.py
+++ src/server.py
@@ -1,8 +1,12 @@
 from collections import OrderedDict
+import logging
 import os
+import queue
+import threading
 import traceback
+import uuid
 
-from flask import Flask, json, jsonify, request
+from flask import Flask, json, jsonify, request, stream_with_context, Response
 
 from config_generator.config_generator import ConfigGenerator
 
@@ -11,14 +15,14 @@ from config_generator.config_generator import ConfigGenerator
 app = Flask(__name__)
 
 
-def config_generator(tenant, use_cached_project_metadata, force_readonly_datasets=False):
+def config_generator(tenant, logger, use_cached_project_metadata, force_readonly_datasets=False):
     """Create a ConfigGenerator instance.
 
     :param str tenant: Tenant ID
     """
     if tenant is None:
         msg = "No tenant selected"
-        app.logger.error(msg)
+        logger.error(msg)
         raise Exception(msg)
 
     # read ConfigGenerator config file
@@ -35,44 +39,68 @@ def config_generator(tenant, use_cached_project_metadata, force_readonly_dataset
             config = json.load(f, object_pairs_hook=OrderedDict)
     except Exception as e:
         msg = "Error loading ConfigGenerator config:\n%s" % e
-        app.logger.error(msg)
+        logger.error(msg)
         raise Exception(msg)
 
     # create ConfigGenerator
-    return ConfigGenerator(config, app.logger, config_file_dir, use_cached_project_metadata, force_readonly_datasets)
+    return ConfigGenerator(config, logger, config_file_dir, use_cached_project_metadata, force_readonly_datasets)
 
 
 # routes
 @app.route("/generate_configs", methods=['POST'])
 def generate_configs():
     """Generate service configs and permissions."""
-    log_output = ""
-    try:
-        # create ConfigGenerator
-        tenant = request.args.get("tenant")
-        use_cached_project_metadata = str(request.args.get("use_cached_project_metadata", "")).lower() in ["1","true"]
-        force_readonly_datasets = str(request.args.get("force_readonly_datasets", "")).lower() in ["1","true"]
-        generator = config_generator(tenant, use_cached_project_metadata, force_readonly_datasets)
-        generator.write_configs()
-        generator.write_permissions()
-        generator.cleanup_temp_dir()
-        logger = generator.get_logger()
+    log_queue = queue.Queue()
+    class StreamingLogHandler(logging.Handler):
+        def emit(self, record):
+            # Log to main logger
+            app.logger.log(record.levelno, record.msg)
 
-        for entry in logger.log_entries():
-            level = entry["level"].upper()
+            level = record.levelname.upper()
             if level == "CRITICAL":
-                log_output += '<b style="color: red">CRITICAL: %s</b>\n' % str(entry["msg"])
+                log_output = '<b style="color: red">CRITICAL: %s</b>' % str(record.msg)
             elif level == "ERROR":
-                log_output += '<span style="color: red">ERROR: %s</span>\n' % str(entry["msg"])
+                log_output = '<span style="color: red">ERROR: %s</span>' % str(record.msg)
             elif level == "WARNING":
-                log_output += '<span style="color: orange">WARNING: %s</span>\n' % str(entry["msg"])
+                log_output = '<span style="color: orange">WARNING: %s</span>' % str(record.msg)
             else:
-                log_output += level + ": " + str(entry["msg"]) + "\n"
+                log_output = level + ": " + str(record.msg)
+            log_queue.put(log_output)
 
-        return (log_output, 200)
-    except Exception as e:
-        return (log_output + "\n\nPython Exception: " + str(e) + "\n" + traceback.format_exc(), 500)
+    stream_handler = StreamingLogHandler()
+    logger = logging.getLogger(str(uuid.uuid4()))
+    logger.setLevel(app.logger.getEffectiveLevel())
+    logger.addHandler(stream_handler)
 
+    config_generator_running = threading.Event()
+    config_generator_running.set()
+
+    def run_config_generator(args):
+        tenant = args.get("tenant")
+        use_cached_project_metadata = str(args.get("use_cached_project_metadata", "")).lower() in ["1","true"]
+        force_readonly_datasets = str(args.get("force_readonly_datasets", "")).lower() in ["1","true"]
+        generator = config_generator(tenant, logger, use_cached_project_metadata, force_readonly_datasets)
+        try:
+            generator.write_configs()
+            generator.write_permissions()
+            generator.cleanup_temp_dir()
+        except Exception as e:
+            logger.error("<b>Python Exception: %s\n%s</b>" % (str(e), traceback.format_exc()))
+
+        config_generator_running.clear()
+
+    threading.Thread(target=run_config_generator, args=[request.args]).start()
+
+    def log_stream():
+        while config_generator_running.is_set():
+            try:
+                # Wait for a log message (timeout avoids hanging)
+                log = log_queue.get(timeout=1)
+                yield log + "\n"
+            except queue.Empty:
+                pass
+
+    return Response(stream_with_context(log_stream()), mimetype='text/plain')
 
 @app.route("/maps", methods=['GET'])
 def maps():
@@ -81,7 +109,7 @@ def maps():
         # get maps from ConfigGenerator
         tenant = request.args.get('tenant')
         use_cached_project_metadata = str(request.args.get("use_cached_project_metadata", "")).lower() in ["1","true"]
-        generator = config_generator(tenant, use_cached_project_metadata)
+        generator = config_generator(tenant, app.logger, use_cached_project_metadata)
         maps = generator.maps()
         generator.cleanup_temp_dir()
 
@@ -97,7 +125,7 @@ def map_details(map_name):
         # get maps from ConfigGenerator
         tenant = request.args.get('tenant')
         use_cached_project_metadata = str(request.args.get("use_cached_project_metadata", "")).lower() in ["1","true"]
-        generator = config_generator(tenant, use_cached_project_metadata)
+        generator = config_generator(tenant, app.logger, use_cached_project_metadata)
         map_details = generator.map_details(map_name)
         generator.cleanup_temp_dir()
 
@@ -116,7 +144,7 @@ def resources():
         # get maps from ConfigGenerator
         tenant = request.args.get('tenant')
         use_cached_project_metadata = str(request.args.get("use_cached_project_metadata", "")).lower() in ["1","true"]
-        generator = config_generator(tenant, use_cached_project_metadata)
+        generator = config_generator(tenant, app.logger, use_cached_project_metadata)
         maps = generator.maps()
         for map_name in maps:
             maps_details.append(generator.map_details(
