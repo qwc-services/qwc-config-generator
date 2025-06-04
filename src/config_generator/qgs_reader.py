e0d65fb6ce439d11e390e068c7ad1ddc319eb585
Sandro Mani <manisandro@gmail.com>
2025-06-04 15:45:42 +0200

    Add fallback for absent filterOpt value attr, as __parse_fileextensions expects a string

--- src/config_generator/qgs_reader.py
+++ src/config_generator/qgs_reader.py
@@ -571,7 +571,7 @@ class QGSReader:
 
         elif edit_widget.get("type") == "ExternalResource":
             filterOpt = edit_widget.find("config/Option/Option[@name='FileWidgetFilter']")
-            constraints['fileextensions'] = self.__parse_fileextensions(filterOpt.get('value')) if filterOpt is not None else ""
+            constraints['fileextensions'] = self.__parse_fileextensions(filterOpt.get('value', "")) if filterOpt is not None else ""
         elif edit_widget.get('type') == 'Hidden':
             constraints['hidden'] = True
             constraints['readOnly'] = True
