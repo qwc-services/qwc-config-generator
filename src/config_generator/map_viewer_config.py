94e3777bab26cd73bc4ff628be9589bd71565415
Sandro Mani <manisandro@gmail.com>
2025-12-15 17:53:27 +0100

    Preserve obliqueDatasets entries in theme config items

--- src/config_generator/map_viewer_config.py
+++ src/config_generator/map_viewer_config.py
@@ -587,6 +587,7 @@ class MapViewerConfig(ServiceConfig):
         self.set_optional_config(cfg_item, 'layerTreeHiddenSublayers', item)
         self.set_optional_config(cfg_item, 'predefinedFilters', item)
         self.set_optional_config(cfg_item, 'map3d', item)
+        self.set_optional_config(cfg_item, 'obliqueDatasets', item)
         self.set_optional_config(cfg_item, 'startupView', item)
 
         if not cfg_item.get('wmsOnly', False):
